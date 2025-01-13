a26443b77f57afccb650fdc00dd98d669f2da9e7
CalebCouch <calebblackcat@gmail.com>
2025-01-13 14:57:51 -0500

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -33,9 +33,76 @@ impl EventHandler<GameError> for State {
         Ok(())
     }
 
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
-        self.0.update(ctx)
-    }
+    fn update(&mut self, ctx: &mut Context) -> GameResult {self.0.update(ctx)}
+
+//  fn mouse_button_down_event(
+//      &mut self, ctx: &mut Context, b: MouseButton, x: f32, y: f32
+//  ) -> GameResult {self.0.mouse_button_down_event(ctx, b, x, y)}
+
+//  fn mouse_button_up_event(
+//      &mut self, ctx: &mut Context, b: MouseButton, x: f32, y: f32
+//  ) -> GameResult {self.0.mouse_button_up_event(ctx, b, x, y)}
+
+//  fn mouse_motion_event(
+//      &mut self, ctx: &mut Context, x: f32, y: f32, dx: f32, dy: f32
+//  ) -> GameResult {self.0.mouse_motion_event(ctx, x, y, dx, dy)}
+
+//  fn mouse_enter_or_leave(
+//      &mut self, ctx: &mut Context, entered: bool
+//  ) -> GameResult {self.0.mouse_enter_or_leave(ctx, entered)}
+
+//  fn mouse_wheel_event(
+//      &mut self, ctx: &mut Context, x: f32, y: f32
+//  ) -> GameResult {self.0.mouse_wheel_event(ctx, x, y)}
+
+//  fn key_down_event(
+//      &mut self, ctx: &mut Context, input: KeyInput, repeated: bool
+//  ) -> GameResult {self.0.key_down_event(ctx, input, repeated)}
+
+//  fn key_up_event(
+//      &mut self, ctx: &mut Context, input: KeyInput
+//  ) -> GameResult {self.0.key_up_event(ctx, input)}
+
+//  fn text_input_event(
+//      &mut self, ctx: &mut Context, character: char
+//  ) -> GameResult {self.0.text_input_event(ctx, character)}
+
+//  fn touch_event(
+//      &mut self, ctx: &mut Context, phase: TouchPhase, x: f64, y: f64
+//  ) -> GameResult {self.0.touch_event(ctx, phase, x, y)}
+
+//  fn gamepad_button_down_event(
+//      &mut self, ctx: &mut Context, btn: Button, id: GamepadId
+//  ) -> GameResult {self.0.gamepad_button_down_event(ctx, btn, id)}
+
+//  fn gamepad_button_up_event(
+//      &mut self, ctx: &mut Context, btn: Button, id: GamepadId
+//  ) -> GameResult {self.0.gamepad_button_up_event(ctx, btn, id)}
+
+//  fn gamepad_axis_event(
+//      &mut self, ctx: &mut Context, axis: Axis, value: f32, id: GamepadId
+//  ) -> GameResult {self.0.gamepad_axis_event(ctx, axis, value, id)}
+
+//  fn focus_event(
+//      &mut self, ctx: &mut Context, gained: bool
+//  ) -> GameResult {self.0.focus_event(ctx, gained)}
+
+
+//  fn resize_event(
+//      &mut self, ctx: &mut Context, width: f32, height: f32
+//  ) -> GameResult {self.0.resize_event(ctx, width, height)}
+
+//  fn quit_event(&mut self, _ctx: &mut Context) -> GameResult<bool> {
+//      //Shut down threads
+//      Ok(true)
+//  }
+
+//  fn on_error(
+//      &mut self, _ctx: &mut Context, _origin: ErrorOrigin, _e: E
+//  ) -> bool {
+//      //Trigger error page
+//      true
+//  }
 
     //TODO: Fill out rest of events
 }
